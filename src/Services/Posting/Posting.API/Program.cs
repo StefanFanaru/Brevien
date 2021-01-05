5a7b76e3a2d211b90e6619962e5b28360fdc464b
Stefan Fanaru <stefan.fanaru@outlook.com>
2021-01-05 23:57:32 +0200

    [Posting] Missing using statement
    Signed-off-by: Stefan Fanaru <stefan.fanaru@outlook.com>

--- src/Services/Posting/Posting.API/Program.cs
+++ src/Services/Posting/Posting.API/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Posting.Infrastructure.Data;
 using Serilog;
 
 namespace Posting.API
